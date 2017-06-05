c2bf4d32529ca8f55c80a0f5271a6e96ff86e057
Wes T4N <wesleyt1998@gmail.com>
2017-06-05 18:34:04 -0400

    file setup

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EM Wireless</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous">
+  </script>
+  <script src="./js/main.js"></script>
+  <link href="./css/bootstrap.min.css" rel="stylesheet" />
+  <script src="https://use.fontawesome.com/104d9a8f78.js"></script>
+  <?php require_once "./includes/functions.php"; ?>
+</head>
+
+<body>
+      <nav class="navbar">
+
+      </nav>
+    <div class="wrapper">
