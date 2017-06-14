84bcf1759444552f8a8a54bf2e5628ddb44632db
Wes T4N <wesleyt1998@gmail.com>
2017-06-14 17:21:15 -0400

    finished service page

--- services.php
+++ services.php
@@ -11,26 +11,79 @@ $nameErr = $subjErr = $msgErr = "";
       <div class="center-block banner service-banner">
         <h1 class="header service-header">Our Services</h1>
         <div class="container-fluid row service-row">
-          <div class="container-fluid col-md-4 service-col">
+          <div class="container-fluid col-md-6 service-col">
             <div class="column-header-logo">
               <i class="fa fa-usd" aria-hidden="true"></i>
             </div>
-            <h2 class="subheader service-subheader">Smartphone Sales</h2>
-            <div class="row phone-row">
-              <img src="./img/galaxy.svg" alt="Galaxy phone icon">
+            <h2 class="subheader service-subheader">Smartphone and Accessory Sales</h2>
+            <div class="row">
+              <div class="col-xs-3 icon-col">
+                <img class="section-icon" id="iphone" src="./img/iphone.svg" alt="iphone"/>
+              </div>
+              <div class="col-xs-9">
+                <h2 class="section-subheader">Apple</h2>
+                <p class="description">
+                  iPhone 5, 5s, 5c
+                  <br />
+                  iPhone 6, 6 Plus, 6s, 6s Plus
+                  <br />
+                  iPhone 7, 7 Plus
+                </p>
+              </div>
+            </div>
+            <hr />
+            <div class="row">
+              <div class="col-xs-3 icon-col">
+                <img class="section-icon center-block" id="galaxy" src="./img/galaxy.svg" alt="galaxy phone"/>
+              </div>
+              <div class="col-xs-9">
+                <h2 class="section-subheader">Samsung</h2>
+                <p class="description">
+                  Galaxy S and Note Series.
+                </p>
+              </div>
+            </div>
+            <hr />
+            <div class="row bottom-row">
+              <div class="col-xs-3 icon-col">
+                <img class="section-icon center-block" id="dots" src="./img/dots.svg" alt="dots"/>
+              </div>
+              <div class="col-xs-9">
+                <h2 class="section-subheader">Accessories</h2>
+                <p class="description">
+                  Phone cases, chargers and more!
+                </p>
+              </div>
             </div>
           </div>
-          <div class="container-fluid col-md-4 service-col">
+          <div class="container-fluid col-md-6 service-col">
             <div class="column-header-logo">
               <i id="wrench" class="fa fa-wrench" aria-hidden="true"></i>
             </div>
-            <h2 class="subheader service-subheader">Smartphone Repairs</h2>
-          </div>
-          <div class="container-fluid col-md-4 service-col">
-            <div class="column-header-logo">
-              <i class="fa fa-cog" aria-hidden="true"></i>
+            <h2 class="subheader service-subheader">Smartphone Repairs and Repair Supplies</h2>
+            <div class="row">
+              <div class="col-xs-3">
+                <img class="section-icon center-block" id="screwdriver" src="./img/screwdriver.svg" alt="screwdriver"/>
+              </div>
+              <div class="col-xs-9">
+                <h2 class="section-subheader">Repairs</h2>
+                <p class="description">
+                  Troubleshooting <br /> Screen Replacement <br /> Battery Replacement <br /> Component Replacement
+                </p>
+              </div>
             </div>
-            <h2 class="subheader service-subheader">DIY Smartphone Supplies</h2>
+            <hr />
+            <div class="row bottom-row">
+              <div class="col-xs-3">
+                <img class="section-icon center-block" id="gear" src="./img/gear.svg" alt="gear"/>
+              </div>
+              <div class="col-xs-9">
+                <h2 class="section-subheader">DIY Supplies</h2>
+                <p class="description">
+                  Phone Screens <br /> Logic Boards <br /> Batteries <br /> Camera Modules <br /> Rear Cases <br /> etc.
+                </p>
+              </div>
+            </div>
           </div>
 
         </div>
@@ -40,15 +93,15 @@ $nameErr = $subjErr = $msgErr = "";
       <!-- Wrapper for slides -->
       <div class="carousel-inner">
         <div class="item active">
-          <img src="https://unsplash.it/960/540?image=816&blur" alt="Los Angeles">
+          <img src="https://unsplash.it/960/540?image=816&blur" alt="Placeholder Background">
         </div>
 
         <div class="item">
-          <img src="https://unsplash.it/960/540?image=816&blur" alt="Los Angeles">
+          <img src="https://unsplash.it/960/540?image=816&blur" alt="Placeholder Background">
         </div>
 
         <div class="item">
-          <img src="https://unsplash.it/960/540?image=816&blur" alt="Los Angeles">
+          <img src="https://unsplash.it/960/540?image=816&blur" alt="Placeholder Background">
         </div>
       </div>
     </div>
