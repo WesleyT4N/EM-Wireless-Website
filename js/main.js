fc784b6274c56c4247425f27a961f9feeaf7f77b
Wes T4N <wesleyt1998@gmail.com>
2017-06-19 17:10:16 -0400

    nav hides when scrolling down

--- js/main.js
+++ js/main.js
@@ -2,6 +2,50 @@
 $('.carousel').carousel();
 
 $(document).ready(function() {
+
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('.navbar').outerHeight();
+
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('.navbar').addClass('nav-up');
+          console.log('added');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('.navbar').removeClass('nav-up');
+              console.log("removed");
+          }
+      }
+
+      lastScrollTop = st;
+  }
+
+
+
   $('.form-input').focus(function(){
     var label = $(this).prev();
     $(label).addClass("animate-label");
@@ -61,6 +105,7 @@ $(document).ready(function() {
     }
   });
 });
+
 
 
 function validateContactForm() {
