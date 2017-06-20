17db4344732c2a075d22c2fadd6e2d73e793f7ca
Wes T4N <wesleyt1998@gmail.com>
2017-06-20 16:52:13 -0400

    fixed navbar

--- includes/header.php
+++ includes/header.php
@@ -8,11 +8,11 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="./css/style.css"/>
-  <script src="./js/main.js"></script>
+  <link rel="stylesheet" type="text/css" href="./css/style.min.css"/>
+  <script src="./js/main.min.js"></script>
   <script src="https://use.fontawesome.com/104d9a8f78.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Fira+Sans|Raleway" rel="stylesheet">
-  
+
   <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -30,7 +30,7 @@
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <i class="fa fa-bars" aria-hidden="true"></i>
       </button>
-      <a href="./home" class="navbar-brand">
+      <a href="./" class="navbar-brand">
         <span class="large-logo-part"><i class="fa fa-mobile" aria-hidden="true"></i> EM</span>
         <span class="small-logo-part">Wireless</span>
       </a>
@@ -38,7 +38,7 @@
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="./home"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+          <a class="nav-link" href="./"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="./services"><i class="fa fa-wrench" aria-hidden="true"></i> Our Services</a>
